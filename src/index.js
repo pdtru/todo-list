6370c7c63ca7b5b92988c24b17fc3889d4b60ff4
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-08-23 13:02:24 -0700

    Add favicon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import _ from 'lodash';
 import './styles.css';
 import Footer from './components/Footer';
+import favicon from '/src/assets/images/logo.png?v=2';
+
+function setFavicons(favImg) {
+  let headTitle = document.querySelector('head');
+  let setFavicon = document.createElement('link');
+  setFavicon.setAttribute('rel', 'shortcut icon');
+  setFavicon.setAttribute('href', favImg);
+  headTitle.appendChild(setFavicon);
+}
+setFavicons(favicon);
 
 const body = document.body;
 const footer = new Footer();
