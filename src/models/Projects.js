7bfefd254780f899173d11bde4e4fe7459c2a3b8
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-09-14 09:27:13 -0700

    Remove unused code

--- src/models/Projects.js
+++ src/models/Projects.js
@@ -1,15 +1,10 @@
 import Cache from '../controllers/Cache';
-import AppState from '../stores/AppState';
-import Project from './Project';
 
 class Projects {
   projects = new Map();
 
   initialize = () => {
     this.projects = Cache.getProjects();
-    // const currentProject = new Project('Today');
-    // this.projects.set(currentProject.id, currentProject);
-    // AppState.currentProject = currentProject;
   };
 
   deleteProject = (id) => {
